e285ef7dc1cccb0083f976dabddae58f79e85e30
Sebastian Opałczyński <opalczynski@gmail.com>
2016-11-05 16:56:23 +0100

    [DEV things] cleanup;

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='sprintero',
-    version='0.1.1',
+    version='0.1.2',
     description='Sprint name generator',
     long_description=README,
     author='Sebastian Opalczynski',
--- sprintero/main.py
+++ sprintero/main.py
@@ -2,9 +2,6 @@
 
 import click
 
-import sys
-sys.path.append('/home/sopalczy/projects/sprintero-core')
-
 from sprintero.commands import top_level
 
 cli = click.CommandCollection(
@@ -13,7 +10,4 @@ cli = click.CommandCollection(
 
 
 def main():
-    cli(obj={})
-
-if __name__ == '__main__':
     cli(obj={})
